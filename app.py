fe8ca9a3061d565627a2772b805dad27a2453b5e
TANGP333 <3526992363@qq.com>
2026-03-30 23:01:48 +0800

    v3.2: 深色模式 + Vercel部署配置

--- app.py
+++ app.py
@@ -201,4 +201,4 @@ def get_analysis(resume, jd):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=7860)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 7860)))
